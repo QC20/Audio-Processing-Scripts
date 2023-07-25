62c6c35477730f57b45f27d4c6e1ae7a768413f2
Jonas <36644388+QC20@users.noreply.github.com>
2023-07-25 22:13:29 +0200

    Add files via upload

---
+++ compress-mp3.py
@@ -0,0 +1,18 @@
+from pydub import AudioSegment
+
+def compress_mp3(input_file, output_file, bitrate='1k'):
+    # Load the input MP3 file
+    audio = AudioSegment.from_file(input_file, format="mp3")
+
+    # Set the desired output bitrate
+    audio.export(output_file, format="mp3", bitrate=bitrate)
+
+    print(f"Compressed MP3 saved as {output_file}")
+
+# Provide the input file path, output file path, and desired bitrate
+input_file = "/Users/jonaskjeldmand/Desktop/long.mp3"
+output_file = "/Users/jonaskjeldmand/Desktop/try.mp3"
+bitrate = '1k'  # Adjust the bitrate as per your requirement   
+
+# Call the function to compress the MP3 file
+compress_mp3(input_file, output_file, bitrate)
